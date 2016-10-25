316147ae17f6574f19843bf1baa4185cd878fb83
Nick <njwest9@gmail.com>
2016-10-25 12:29:14 -0400

    easter egg functionality added ;)

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,6 +9,7 @@ import ReactGridLayout from 'react-grid-layout';
 import RaisedButton from 'material-ui/RaisedButton';
 import { Grid, Row, Col } from 'react-flexbox-grid/lib/index';
 import { Card } from 'material-ui/Card';
+import Konami from 'react-konami';
 
 class _Home extends Component {
   constructor(props) {
@@ -28,31 +29,43 @@ class _Home extends Component {
     };
   }
 
+
   componentDidMount() {
   }
 
   render() {
+    const Slappy = () =>{
+      console.log('aww yeah');
+    }
+
     return (
       <div>
+      <Konami easterEgg={Slappy} />
       <Toolbar>
         <div className="container">
           <ToolbarTitle text="About Nick"/>
         </div>
       </Toolbar>
+
+        <easterEgg/>
         <div className="container">
 
           <Row>
             <Col xs={12} md={7}>
-              <Card>
-                <h2>Sup suckas</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <Card className="card">
+                <div className="cardContent">
+                  <h2>Sup suckas</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
               </Card>
             </Col>
 
             <Col xs={12} md={5}>
-              <Card>
-                <h2>Mo stuff</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <Card className="card">
+                <div className="cardContent">
+                  <h2>Mo stuff</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
               </Card>
             </Col>
           </Row>
